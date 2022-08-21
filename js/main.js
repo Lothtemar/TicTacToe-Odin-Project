cd0d979067695bd7b0cc20d618cf762d4da29835
Loth <gendar@yahoo.com>
2022-08-21 13:56:04 +0200

    JS update 0.3

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-// Global Variables
+// GLOBAL VARIABLES
 const startBtn = document.querySelector("#new-game-btn");
 const board = document.querySelector(".container");
 const sqareCell = document.querySelectorAll("[data-cell]");
+const popUpResult = document.querySelector(".winner");
+const resultGame = document.querySelector("[data-winner]");
+const btnRestart = document.getElementById("restart-btn");
 const x_class = "x";
 const o_class = "o";
 let playerTurn = true;
+const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
-sqareCell.forEach(square => {
-    square.addEventListener("click", squareClick, {once: true});
-});
-
+// GAME FUNCTIONALITY
 
 function squareClick(e){
     playerTurn ? playerPlays(e) : aiPlays(e);
@@ -23,8 +33,11 @@ function playerPlays(e){
     const square = e.target;
     const currentTurn = playerTurn ? x_class : o_class;
     square.classList.add(currentTurn);
-    playerTurn = false;
-}
+    if (checkWinner(currentTurn)){
+        isWin(currentTurn);
+    };
+    playerTurn = !playerTurn;
+};
 
 function aiPlays(e){
     if(board.classList.contains(o_class)) {
@@ -34,11 +47,58 @@ function aiPlays(e){
     const square = e.target;
     const currentTurn = playerTurn ? x_class : o_class;
     square.classList.add(currentTurn);
-    playerTurn = true;
+    if (checkWinner(currentTurn)){
+        isWin(currentTurn);
+    };
+    playerTurn = !playerTurn;
 };
 
+// RESULT GAME FUNCTIONS
+
+function checkWinner(condition){
+    if (isDraw()){
+        popUpResult.classList.add("show");
+        resultGame.textContent = "A draw it is!!!!";
+        btnRestart.addEventListener("click", () => {
+            popUpResult.classList.remove("show");
+            start();
+        });
+    } else {
+        return winningCombinations.some(combination => {
+            return combination.every(index => {
+                return sqareCell[index].classList.contains(condition)
+            });
+        });
+    }
+};
+
+function isWin(active_player){
+    popUpResult.classList.add("show");
+    resultGame.textContent = `${active_player.toUpperCase()} wins!`;
+    btnRestart.addEventListener("click", ()=> {
+        popUpResult.classList.remove("show");
+        start();
+    });
+};
+
+function isDraw(){
+    return [...sqareCell].every(squareFill => {
+        return squareFill.classList.contains(x_class) || 
+            squareFill.classList.contains(o_class);
+    })
+};
+
+// TO START GAME
+
 function start(){
-    console.log("clicked")
+    sqareCell.forEach(square => {
+        square.classList.remove(x_class);
+        square.classList.remove(o_class);
+    });
+    sqareCell.forEach(square => {
+        square.addEventListener("click", squareClick, {once: true});
+    });
+
 };
 
 startBtn.onclick = start;
