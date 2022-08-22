0eb5ae5fe310e406eb04355d4d4ab50daf8a3818
Loth <gendar@yahoo.com>
2022-08-22 11:56:56 +0200

    HTML CSS JS update 0.4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 // GLOBAL VARIABLES
 const startBtn = document.querySelector("#new-game-btn");
+const selectMenu = document.querySelector(".layer")
 const board = document.querySelector(".container");
 const sqareCell = document.querySelectorAll("[data-cell]");
 const popUpResult = document.querySelector(".winner");
 const resultGame = document.querySelector("[data-winner]");
 const btnRestart = document.getElementById("restart-btn");
+const popUpOptions = document.querySelector(".popup-choose");
+const pvp_card = document.querySelector(".pvp");
+const pvai_card = document.querySelector(".pvai");
+const x_card = document.querySelector(".x-card");
+const o_card = document.querySelector(".o-card");
 const x_class = "x";
 const o_class = "o";
-let playerTurn = true;
+let aI = true;
+let xTurn = true;
+let parameters = [true, true];
 const winningCombinations = [
     [0,1,2],
     [3,4,5],
@@ -19,10 +27,11 @@ const winningCombinations = [
     [2,4,6]
 ];
 
+
 // GAME FUNCTIONALITY
 
 function squareClick(e){
-    playerTurn ? playerPlays(e) : aiPlays(e);
+    xTurn ? playerPlays(e) : aiPlays(e);
 };
 
 function playerPlays(e){
@@ -31,12 +40,12 @@ function playerPlays(e){
         board.classList.add(o_class)
     };
     const square = e.target;
-    const currentTurn = playerTurn ? x_class : o_class;
+    const currentTurn = xTurn ? x_class : o_class;
     square.classList.add(currentTurn);
     if (checkWinner(currentTurn)){
         isWin(currentTurn);
     };
-    playerTurn = !playerTurn;
+    xTurn = !xTurn;
 };
 
 function aiPlays(e){
@@ -45,18 +54,19 @@ function aiPlays(e){
         board.classList.add(x_class)
     };
     const square = e.target;
-    const currentTurn = playerTurn ? x_class : o_class;
+    const currentTurn = xTurn ? x_class : o_class;
     square.classList.add(currentTurn);
     if (checkWinner(currentTurn)){
         isWin(currentTurn);
     };
-    playerTurn = !playerTurn;
+    xTurn = !xTurn;
 };
 
 // RESULT GAME FUNCTIONS
 
 function checkWinner(condition){
     if (isDraw()){
+        selectMenu.classList.remove("hidden");
         popUpResult.classList.add("show");
         resultGame.textContent = "A draw it is!!!!";
         btnRestart.addEventListener("click", () => {
@@ -73,6 +83,7 @@ function checkWinner(condition){
 };
 
 function isWin(active_player){
+    selectMenu.classList.remove("hidden");
     popUpResult.classList.add("show");
     resultGame.textContent = `${active_player.toUpperCase()} wins!`;
     btnRestart.addEventListener("click", ()=> {
@@ -88,13 +99,82 @@ function isDraw(){
     })
 };
 
-// TO START GAME
+// GAME OPTIONS MENU
 
-function start(){
+function popUpGameMode(){
+    selectMenu.classList.remove("hidden");
+    selectMenu.classList.add("show");
+    if(popUpOptions.classList.contains("hidden")){
+        popUpOptions.classList.remove("hidden");
+    };
+
+    parameters[0] === true ? pvai_card.classList.add("selected") : 
+        pvp_card.classList.add("selected");
+    parameters[1] === true ? x_card.classList.add("selected") :
+    o_card.classList.add("selected")
+
+
+    document.querySelectorAll(".card").forEach(card => {
+        card.addEventListener("click", (e) => controls(e));
+    });
+};
+
+function controls(e){
+
+        if (e.target.id === "x1" | e.target.id === "x2"){
+            if (e.target.id === "x1"){
+                parameters[0] = false;
+                e.target.classList.add("selected")
+                if (pvai_card.classList.contains("selected")){
+                    pvai_card.classList.remove("selected");
+                } 
+            } else {
+                parameters[0] = true;
+                e.target.classList.add("selected");
+                pvp_card.classList.remove("selected");
+            }
+        } else if (e.target.id === "x3" | e.target.id === "x4" ){
+            if(e.target.id === "x3"){
+                parameters[1] = true; 
+                e.target.classList.add("selected");
+                if (o_card.classList.contains("selected")){
+                    o_card.classList.remove("selected");
+                } 
+            } else {
+                parameters[1] = false
+                e.target.classList.add("selected");
+                x_card.classList.remove("selected");
+            }
+        } else {
+            selectMenu.classList.remove("show");
+            selectMenu.classList.add("hidden");
+            popUpOptions.classList.add("hidden");
+            setUp(parameters);
+        }
+    }
+
+function setUp(parameters){
+    console.log(parameters);
+    parameters[0] === true ? aI = true : aI = false;
+    parameters[1] === true ? xTurn = true : xTurn = false;
+    xTurn === true ? board.classList.add(x_class) : board.classList.add(o_class);
+};
+
+// CLEAN BOARD AND SQUARE
+
+function cleanGame(){
     sqareCell.forEach(square => {
         square.classList.remove(x_class);
         square.classList.remove(o_class);
     });
+    board.classList.remove(x_class, o_class);
+}
+
+// TO START GAME
+
+function start(){
+    cleanGame();
+    popUpGameMode();
     sqareCell.forEach(square => {
         square.addEventListener("click", squareClick, {once: true});
     });
